cf7ee343ab23bd09152d9b1a7c71d5aa4e793901
unknown <csmcc2@hotmail.com>
2023-01-12 15:33:58 +0800

    format

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -5,6 +5,7 @@ import Container from '@mui/material/Container'
 import TextField from '@mui/material/TextField'
 import dayjs, { Moment } from 'moment'
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker'
+import { PickersDay } from '@mui/x-date-pickers/PickersDay'
 
 export default function Calendar() {
   const [today, setToday] = useState<Moment | null>(dayjs(new Date()))
@@ -20,6 +21,14 @@ export default function Calendar() {
           disableOpenPicker
           displayStaticWrapperAs="desktop"
           value={today}
+          dayOfWeekFormatter={(day: string) => day}
+          renderDay={(day, selectedDays, pickersDayProps) => {
+            var week = day.isoWeekday()
+            if (week === 6 || week === 7) {
+              return <PickersDay sx={{ color: "red" }} {...pickersDayProps} />
+            }
+            return <PickersDay {...pickersDayProps} />
+          }}
           onChange={(newValue) => { setToday(newValue) }}
           renderInput={(params) => <TextField {...params} />}
         />
